22dc91ec0a3ef4b0d3a6cc51095121737259f3ce
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-20 22:52:51 +0530

    feat: update Home page to use success variant of Button component; simplify README.md

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Button } from '../../../packages/stackedui/src/components/button'
+import { Button } from 'stackedui'
 const Home = () => {
   return (
     <div>
       <h1 className='text-3xl'>Stack UI docs</h1>
-
-      <Button>Default Button</Button>
+      <Button variant={'success'} className='mt-4'>Success Button</Button>
     </div>
   )
 }
