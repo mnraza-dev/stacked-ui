1d25001a7ffab24cf6d08bdfdb430a6c9a329471
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-20 22:23:03 +0530

    feat: update Home page to use Button component from stackedui; remove old index.tsx file and adjust paths in tsconfig.json

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { Button } from '../../../packages/stackedui/src/components/button'
 const Home = () => {
   return (
     <div>
       <h1 className='text-3xl'>Stack UI docs</h1>
+
+      <Button>Default Button</Button>
     </div>
   )
 }
--- packages/stackedui/vite.config.ts
+++ packages/stackedui/vite.config.ts
@@ -1,14 +1,37 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import dts from 'vite-plugin-dts'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), 
+    dts({ insertTypesEntry: true }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'StackedUI',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
 })
