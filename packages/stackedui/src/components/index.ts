6171bd0e637fe48447a896d5fdff6d87dc327f72
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-22 22:04:20 +0530

    feat: add Input component with variant support and styling

--- packages/stackedui/src/components/index.ts
+++ packages/stackedui/src/components/index.ts
@@ -1,1 +1,40 @@
-export * from "./button/Button"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const inputVariants = cva(
+  "flex h-10 w-full rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-900 px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "hover:border-indigo-400 dark:hover:border-indigo-600",
+        glowing:
+          "border-2 border-transparent bg-gradient-to-r from-indigo-600/10 to-pink-600/10 hover:bg-gradient-to-r hover:from-indigo-600/20 hover:to-pink-600/20 focus:ring-pink-500",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, variant, type = "text", ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(inputVariants({ variant, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export { Input, inputVariants };
