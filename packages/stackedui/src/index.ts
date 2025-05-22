e1a53561731f79c973ca00bf5febf688a0e6725a
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-22 22:42:14 +0530

    feat: export Carousel component from index file

--- packages/stackedui/src/index.ts
+++ packages/stackedui/src/index.ts
@@ -2,3 +2,4 @@ export * from "./components/button/Button";
 export * from "./components/input/Input";
 export * from "./components/card/Card";
 export * from "./components/accordion/Accordion";
+export * from "./components/carousel/carousel";
